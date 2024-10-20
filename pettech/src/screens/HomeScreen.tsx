ede327fcb99862849a4d9feb09b1741a8672c17f
Aldairm0308 <al222111405@gmail.com>
2024-10-19 23:12:50 -0600

    HoraDisplay added

--- pettech/src/screens/HomeScreen.tsx
+++ pettech/src/screens/HomeScreen.tsx
@@ -4,6 +4,7 @@ import React, { useState } from "react";
 import { View, Text, TouchableOpacity, Switch } from "react-native";
 import { useTheme } from "./../hooks/useTheme"; // Importa el hook useTheme
 import BreedCard from "../components/BreedCard";
+import HoraDisplay from "../components/HoraActual";
 
 const HomeScreen = () => {
   const { theme, toggleTheme } = useTheme();
@@ -12,7 +13,7 @@ const HomeScreen = () => {
     <View style={[theme.styles.container]}>
       <View style={[theme.styles.containerSet]}>
         <Text style={[theme.text]}>Home</Text>
-        <BreedCard />
+        <HoraDisplay />
       </View>
     </View>
   );
